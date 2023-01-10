3f1de565a82e4485bec64bafa91626ada3c8df1f
UlulAzmi111 <azmiulul1453@gmail.com>
2023-01-10 13:36:00 +0700

    Menambahkan file json.js dan latihan.json untuk video ke 27

---
+++ js/json.js
@@ -0,0 +1,25 @@
+let tblmenu = [
+    {
+    "idmenu" : 35,
+    "idkategori" : 14,
+    "menu" : "Apel Manalagi",
+    "harga" : 3000,
+    "warna" : ["merah", "kuning", "hijau"],
+    "stok" : true,
+    "keterangan" : null
+
+}, 
+{
+    "idmenu" : 36,
+    "idkategori" : 14,
+    "menu" : "Pisang Raja",
+    "harga" : 5000,
+    "warna" : ["kuning", "hijau"],
+    "stok" : false,
+    "keterangan" : null
+
+}
+
+]
+
+console.log(tblmenu[0].menu);
