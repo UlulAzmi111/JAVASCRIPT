b30c80586c764631888159eb3bc03e05d95b9bf4
UlulAzmi111 <azmiulul1453@gmail.com>
2023-01-16 10:17:21 +0700

    Memodifikasi file untuk video ke 28

--- js/json.js
+++ js/json.js
@@ -1,25 +1,48 @@
-let tblmenu = [
-    {
-    "idmenu" : 35,
-    "idkategori" : 14,
-    "menu" : "Apel Manalagi",
-    "harga" : 3000,
-    "warna" : ["merah", "kuning", "hijau"],
-    "stok" : true,
-    "keterangan" : null
+document.querySelector("#klik").addEventListener("click", tampil);
 
-}, 
-{
-    "idmenu" : 36,
-    "idkategori" : 14,
-    "menu" : "Pisang Raja",
-    "harga" : 5000,
-    "warna" : ["kuning", "hijau"],
-    "stok" : false,
-    "keterangan" : null
+function tampil(params) {
+    let url = "js/tblmenu.json";
+    fetch(url)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        let output = "<h1>DATA MENU</h1> <table><th>Menu</th><th>Harga</th><th>Warna</th>";
+        data.forEach(element => {
+            output += `<tr>
+                <td>${element.menu}</td>
+                <td>${element.harga}</td>
+                <td>${element.warna[0]}</td>
+            </tr>`;
+        });
+        output += "</table>";
+        document.querySelector("#isi").innerHTML = output;
+      })
 
 }
 
-]
+// let tblmenu = [
+//     {
+//     "idmenu" : 35,
+//     "idkategori" : 14,
+//     "menu" : "Apel Manalagi",
+//     "harga" : 3000,
+//     "warna" : ["merah", "kuning", "hijau"],
+//     "stok" : true,
+//     "keterangan" : null
 
-console.log(tblmenu[0].menu);
+// }, 
+// {
+//     "idmenu" : 36,
+//     "idkategori" : 14,
+//     "menu" : "Pisang Raja",
+//     "harga" : 5000,
+//     "warna" : ["kuning", "hijau"],
+//     "stok" : false,
+//     "keterangan" : null
+
+// }
+
+// ]
+
+// console.log(tblmenu[0].menu);
