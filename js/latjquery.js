ac42ade18e4069c6a8b8e2e6242d835f639a72b8
UlulAzmi111 <azmiulul1453@gmail.com>
2023-01-23 08:37:03 +0700

    Memodifikasi file latquery.js untuk video ke 32 -> Jquery Event

--- js/latjquery.js
+++ js/latjquery.js
@@ -5,5 +5,15 @@ $( document ).ready(function() {
 
 $(function () {
     $("#isi").html("<h1>Belajar Jquery</h1>");
+
+    $("#klik").click(function (e) { 
+        e.preventDefault();
+        alert("belajar javascript");
+    });
+
+    $("#isi").mouseleave(function () { 
+        alert("mouse leave");
+        console.log("mouse");
+    });
 })
 
